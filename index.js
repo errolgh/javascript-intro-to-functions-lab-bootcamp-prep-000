61f95488425f360cd0fe95a0c6b04c1badce99f2
Errol W <ewatsonrei@gmail.com>
2018-11-29 20:57:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
-
-
-// code is wrong from here
 
 function logShout(string) {
-  return logShout.toUpperCase
+  console.log(string.toUpperCase());
 }
-
-// to here
